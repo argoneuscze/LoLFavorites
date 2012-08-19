9895b73b7cc8be6dff91fae728b7726170d3b86a
Tomas Drbota <argoneuscze@gmail.com>
2012-08-20 00:31:35 +0200

    added license declarations

--- LoLFavorites/Code/AutoUpdate.cs
+++ LoLFavorites/Code/AutoUpdate.cs
@@ -1,4 +1,23 @@
-﻿using System;
+﻿/*
+ * Copyright (C) 2012 Tomáš Drbota
+ * 
+ * This file is part of LoLFavorites.
+
+ * LoLFavorites is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+
+ * LoLFavorites is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+
+ * You should have received a copy of the GNU General Public License
+ * along with LoLFavorites.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -26,20 +45,22 @@ namespace LoLFavorites.Code
         public static void checkUpdates()
         {
             bool champUpdate = checkChampionUpdates();
-            bool appUpdate = checkAppUpdates();
+            //bool appUpdate = checkAppUpdates();
 
-            if (champUpdate && appUpdate)
+            /*if (champUpdate && appUpdate)
             {
                 updateNeeded = 0;
-            }
-            else if (champUpdate && !appUpdate)
+            }*/
+            if (champUpdate)
+                updateNeeded = 1;
+            /*else if (champUpdate && !appUpdate)
             {
                 updateNeeded = 1;
-            }
-            else if (!champUpdate && appUpdate)
+            }*/
+            /*else if (!champUpdate && appUpdate)
             {
                 updateNeeded = 2;
-            }
+            }*/
         }
 
         private static bool checkAppUpdates()
